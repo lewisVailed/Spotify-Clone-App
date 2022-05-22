d9781f806bdba5ed173e0af9b3c79c93ffd106a4
Ayberk Bilgiç <lewidvailed@outlook.com>
2022-05-22 21:48:57 +0300

    Update

--- Spotify-Clone-App/Managers/APICaller.swift
+++ Spotify-Clone-App/Managers/APICaller.swift
@@ -12,7 +12,52 @@ final class APICaller {
     
     private init() {}
     
+    struct Constants {
+        static let baseAPIURL = "https://api.spotify.com/v1"
+    }
+    
+    enum APIError: Error {
+        case failureToGetDate
+    }
+    
     public func getCurrentUserProfile(completion: @escaping (Result<user_profile, Error>) -> Void) {
-        
+        createRequest(with: URL(string: Constants.baseAPIURL + "/me"),
+                      type: .GET) { baseRequest in
+            let task = URLSession.shared.dataTask(with: baseRequest) { data, _, error in
+                guard let data = data, error == nil else {
+                    completion(.failure(APIError.failureToGetDate))
+                    return
+                }
+
+                do {
+                    let result = JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                    print(result)
+                } catch {
+                    completion(.failure(error))
+                }
+                
+            }
+        }
+        }
+    
+    // MARK: - PRİVATE
+    
+    enum HTTPMethod: String {
+        case GET
+        case POST
+    }
+    
+    private func createRequest(with url: URL?, type: HTTPMethod, completion: @escaping (URLRequest) -> Void) {
+        AuthManager.shared.withValidToken { token in
+            guard let apiURL = url else {
+                return
+            }
+            var request = URLRequest(url: apiURL)
+            request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+            request.httpMethod = type.rawValue
+            request.timeoutInterval = 30
+            completion(request)
+            
+        }
     }
 }
